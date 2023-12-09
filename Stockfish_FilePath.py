c31abff09608c467cbe8f48428b4659135bce4b8
sai vishnu anudeep kadiyala <54953858+anudeep-17@users.noreply.github.com>
2023-12-09 11:40:40 -0500

    Update Stockfish_FilePath.py

--- Stockfish_FilePath.py
+++ Stockfish_FilePath.py
@@ -1,2 +1,2 @@
 # please enter your stockfish path here 
-StockFish_FilePath = 
+StockFish_FilePath = "C:\\Users\\anude\\Downloads\\stockfish-windows-x86-64-avx2\\stockfish\\stockfish-windows-x86-64-avx2.exe"
